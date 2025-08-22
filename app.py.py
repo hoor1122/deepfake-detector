a3b282d98483e8c5836d270b721fc7f029ada5f0
hoor1122 <iqramohd507@gmail.com>
2025-08-22 10:10:33 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -224,7 +224,7 @@ if st.session_state.uploaded_image is not None:
     # ====== ANALYZE BUTTON ======
 # ====== ANALYZE BUTTON ======
 # ====== ANALYZE BUTTON (Selected Model)
-
+# ====== ANALYZE BUTTON (Selected Model) ======
 if analyze:
     with st.spinner(f"Analyzing picture with {model_choice}..."):
         progress_bar = st.progress(0)
@@ -233,7 +233,7 @@ if analyze:
         for percent in range(0, 101, 20):
             time.sleep(0.15)
             progress_bar.progress(percent)
-            
+
         class_names = ['Fake', 'Real']
 
         with torch.no_grad():
@@ -242,6 +242,7 @@ if analyze:
             pred_idx = probs.argmax()
             pred_class = class_names[pred_idx]
             confidence = probs[pred_idx] * 100
+
 
         progress_bar.progress(100)
         time.sleep(0.2)
@@ -384,6 +385,7 @@ if clear:
 
 # ====== FOOTER ======
 st.markdown("<div class='footer'>🔍 This result is based on the uploaded image and may not be perfect. Always verify with additional tools.</div>", unsafe_allow_html=True)
+
 
 
 
