1785a360209473e98348c06e5884fba6713a41f0
hoor1122 <iqramohd507@gmail.com>
2025-08-28 10:46:11 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -325,3 +325,4 @@ st.markdown(
     "<div class='footer'>🔍 This result is based on the uploaded image and may not be perfect. Always verify with additional tools.</div>",
     unsafe_allow_html=True
 )
+
